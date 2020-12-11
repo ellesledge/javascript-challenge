e0282f678548d47cd86bbb7bc0580089bc777c84
ellesledge <69279540+ellesledge@users.noreply.github.com>
2020-12-10 20:53:33 -0500

    app

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,34 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+console.log(tableData)
+
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
+
+function ufosighting(data){
+    tbody.html("");
+    data.forEach(trow =>{
+        const row = tbody.append("tr");
+        Object.values(trow).forEach(function(value){
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
+
+function UFO(){
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    var filteredData = tableData.filter(sighting=>sighting.datetime===inputValue);
+
+    // console.log(filteredData);
+    ufosighting(filteredData);
+};
+
+button.on("click",UFO);
+form.on("submit", UFO);
+
+ufosighting(tableData);
